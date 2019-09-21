3b5958fde8afa7d151e3def76dccd8ba5c093875
Julio Cesar da Costa Reis Filho <julio.filho@devmaker.com.br>
2019-09-21 17:49:17 -0300

    first

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(
+  "mongodb+srv://tod:tg36watr@cluster0-gimnf.mongodb.net/test?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+requireDir("./src/models");
+
+app.use("/api", require("./src/routes"));
+
+app.listen(3001);
